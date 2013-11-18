957a96beb5b2e3e5eb4f3e7dbb3edea0e8fc0e26
gubi <gubi.ale@iod.io>
2013-11-18 18:55:35 +0100

    NAS token frienship system (alpha - must remove 'debug' option)

--- API/index.php
+++ API/index.php
@@ -1,13 +1,103 @@
 <?php
 header("Content-type: text/plain;");
-function get_token($key) {
-	return md5(strstr(str_replace(array("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA", "\n"), "", str_replace(array("-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----"), "", $key)), "ID", true));
+class rsa {
+	private function gen_aes_key($dir) {
+		return shell_exec('openssl rand -base64 32 -out ' . $dir . 'aesKey~');
+	}
+	private function rm_aes_key($dir) {
+		return shell_exec('rm ' . $dir . 'aesKey~');
+	}
+	
+	public function get_time_error() {
+		print "Time expired";
+		exit();
+	}
+	public function get_time_limit($limit = false, $show_deadline = false) {
+		if(!isset($limit) || $limit == false || $limit == null) {
+			$data = date("Y-m-d H:i:s");
+		} else {
+			$data = $limit;
+		}
+		
+		list($date, $time) = explode(" ", $data);
+		list($year, $month, $day) = explode("-", $date);
+		list($hour, $minute, $second) = explode(":", $time);
+		
+		if(!isset($limit) || $limit == false || $limit == null) {
+			$deadline = mktime($hour+1, $minute, $second, $month, $day, $year);
+			$to_string = "+1 hour";
+		} else {
+			$deadline = mktime($hour, $minute+1, $second, $month, $day, $year);
+			$to_string = "+3 minute";
+		}
+		$now = time();
+		$difference = $deadline - $now;
+		
+		if(!$show_deadline) {
+			$sec_diff = (intval($difference - ($hours_left * (60 * 60)) - ($mins_left * 60)) > 0) ? true : false;
+		} else {
+			$sec_diff = date('Y-m-d H:i:s', strtotime($to_string, strtotime($data)));
+		}
+		
+		return $sec_diff;
+	}
+	public function get_token($key) {
+		/*
+		SSL KEY STRUCTURE:
+		______________________________________________________________
+		
+			-----BEGIN PUBLIC KEY-----
+			X509 signature + PEM sig + modulus + 'ID' + exponent
+			-----END PUBLIC KEY----- 
+		______________________________________________________________
+		*/
+		return md5(strstr(str_replace(array("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA", "\n"), "", str_replace(array("-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----"), "", $key)), "ID", true));
+	}
+	/**
+	// Simple encrypt and decrypt string.
+	// Thanks to http://stackoverflow.com/a/1289114 :)
+	*/
+	public function simple_encrypt($text) {
+		return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, SALT, $text, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
+	}
+	public function simple_decrypt($text) {
+		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, SALT, base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
+	}
+	public function public_encrypt($dir, $pub_key, $key_to_encrypt, $time_limit = null){
+		if($this->get_time_limit($time_limit, true)) {
+			$this->gen_aes_key($dir);
+			shell_exec('openssl enc -aes-256-cbc -salt -in ' . $dir . $key_to_encrypt . ' -out ' . $dir . 'rsa_2048_pub.enc~ -pass file:' . $dir . 'aesKey~');
+			shell_exec('openssl rsautl -encrypt -inkey ' . $dir . $pub_key . ' -pubin -in ' . $dir . 'aesKey~ -out ' . $dir . 'aesKey.enc~');
+			$encrypted = shell_exec('cat ' . $dir . '/rsa_2048_pub.enc~ | base64 -');
+			$enckey = shell_exec('cat ' . $dir . 'aesKey.enc~ | base64 -');
+			shell_exec('rm ' . $dir . '/rsa_2048_pub.enc~ ' . $dir . 'aesKey.enc~');
+			$this->rm_aes_key($dir);
+			
+			return trim($encrypted) . "::::" . trim($enckey) . "::::" . $this->simple_encrypt($time_limit);
+		} else {
+			return false;
+		}
+	}
+	public function private_decrypt($dir, $priv_key, $crypted_aes, $crypted_key, $time_limit = null){
+		if($this->get_time_limit($time_limit, true)) {
+			shell_exec('echo "' . $crypted_aes . '" | base64 -d - > ' . $dir . 'aesKey.enc~');
+			shell_exec('echo "' . $crypted_key . '" | base64 -d - > ' . $dir . 'rsa_2048_pub.enc~');
+			shell_exec('openssl rsautl -decrypt -inkey ' . $dir . $priv_key . ' -in ' . $dir . 'aesKey.enc~ -out ' . $dir . 'aesKey.decrypted');
+			shell_exec('openssl enc -d -aes-256-cbc -in ' . $dir . 'rsa_2048_pub.enc~ -out ' . $dir . 'rsa_2048_pub.dec~ -pass file:' . $dir . 'aesKey.decrypted');
+			$public_key = shell_exec('cat ' . $dir . '/rsa_2048_pub.dec~'); 
+			shell_exec('rm ' . $dir . '/aesKey.decrypted ' . $dir . '/aesKey.enc~ ' . $dir . 'rsa_2048_pub.enc~ ' . $dir . 'rsa_2048_pub.dec~');
+			
+			return $public_key;
+		} else {
+			return false;
+		}
+	}
 }
 
-//print md5(md5_file("../common/include/conf/trusted/gubi.pem") . date("Y-m-d") . microtime()) . date("Y-m-d H:i:s") . microtime();
+$trusted_dir = "../common/include/conf/trusted/";
+$main_dir = "../common/include/conf/";
 
 if(isset($_GET["token"]) && trim($_GET["token"]) !== "") {
-	$trusted_dir = "../common/include/conf/trusted/";
 	foreach(glob($trusted_dir . "*.pem") as $filename) {
 		$tokens[] = str_replace(array($trusted_dir, ".pem"), "", $filename);
 	}
@@ -27,34 +117,29 @@ if(isset($_GET["token"]) && trim($_GET["token"]) !== "") {
 	}
 } else {
 	require_once("../common/include/funcs/_blowfish.php");
-	/*
-	SSL key structure:
-	--------------------------------------------------------------------------------------------
-	-----BEGIN PUBLIC KEY-----
-	X509 signature + PEM sig + modulus + 'ID' + exponent
-	-----END PUBLIC KEY----- 
-	--------------------------------------------------------------------------------------------
-	*/
-	$trusted_dir = "../common/include/conf/trusted/";
-	$main_dir = "../common/include/conf/";
-	if(isset($_GET["debug"])) {
-		$password = date("Y-m-d_H");
-	} else {
-		$password = date("Y-m-d_H:i");
-	}
+	
+	$rsa = new rsa();
+	
 	if(isset($_GET["make_friend"]) && trim($_GET["make_friend"]) == "true") {
 		if(isset($_GET["debug"])) {
 			print "-----\n";
 			print "| B1 |\n";
 			print "-----\n\n";
 		}
+		
 		// B request
 		$fb = fopen($main_dir . "rsa_2048_pub.pem", "r");
 		$Bkey = fread($fb, 8192);
 		fclose($fb);
 		
-		$Brequest = PMA_blowfish_encrypt($Bkey . "::::" . md5($password), $password);
+		$limit = (isset($_GET["debug"]) ? false : date("Y-m-d H:i:s"));
+		$time_limit = $rsa->get_time_limit($limit);
+		
+		$friendship_request = PMA_blowfish_encrypt(trim($Bkey), $rsa->get_time_limit($limit, true));
+		$Brequest = PMA_blowfish_encrypt($friendship_request . "::::" . $rsa->get_time_limit($limit, true), "Shoot some hoops?");
+		
 		if(isset($_GET["debug"])) {
+			print $rsa->get_time_limit($limit, true) . "\n\n";
 			print "B request:\n" . rawurlencode($Brequest);
 		} else {
 			print rawurlencode($Brequest);
@@ -66,33 +151,41 @@ if(isset($_GET["token"]) && trim($_GET["token"]) !== "") {
 			print "-----\n\n";
 		}
 		
-		$fa = fopen($main_dir . "rsa_2048_pub.pem", "r");
-		$Akey = fread($fa, 8192);
-		fclose($fa);
-		
 		// A parse
-		$resp = explode("::::", PMA_blowfish_decrypt(rawurldecode($_GET["friend_request"]), $password));
-		$Bkey = $resp[0];
-		$md5_pass = $resp[1];
-		if($md5_pass == md5($password)) {
+		$friendship_request = explode("::::", PMA_blowfish_decrypt(rawurldecode($_GET["friend_request"]), "Shoot some hoops?"));
+		$time_limit = $friendship_request[1];
+		
+		if($rsa->get_time_limit($time_limit)) {
+			$resp = PMA_blowfish_decrypt($friendship_request[0], $time_limit);
+			$Bkey = $resp;
+		
 			// A response
-			$Btoken = get_token($Bkey);
-			$password = sha1($Bkey);
-			$encrypted_A_response = rawurlencode(PMA_blowfish_encrypt($Akey . "::::" . $Btoken, $password));
-			if(isset($_GET["debug"])) {
-				print "B key:\n" . $Bkey;
-				print "\n\nB token: " . $Btoken;
-				print "\nA key:\n" . $Akey . "\n\n";
-				print "Encrypted A response:\n" . $encrypted_A_response;
-			} else {
-				print $encrypted_A_response;
-			}
+			$fb = fopen($main_dir . "rsa_2048_pub.pem", "r");
+			$Akey = fread($fb, 8192);
+			fclose($fb);
+			
+			$Atoken = $rsa->get_token($Akey);
+			$Btoken = $rsa->get_token($Bkey);
 			
 			$knower = fopen($trusted_dir . $Btoken . ".pem~", "wb");
-			fwrite($knower, $_SERVER["REMOTE_ADDR"] . "\n\n" . $Bkey);
+			fwrite($knower, $Bkey);
 			fclose($knower);
+			
+			$encrypted_A_response = $rsa->public_encrypt($main_dir, "trusted/"  . $Atoken . ".pem~", "rsa_2048_pub.pem", $time_limit);
+			if($encrypted_A_response) {
+				if(isset($_GET["debug"])) {
+					print "B key:\n" . $Bkey;
+					print "\n\nB token: " . $Btoken . "\n\n";
+					print "Encrypted A response:\n" . $encrypted_A_response . "\n\n";
+					print "Urlencoded A response:\n" . rawurlencode($encrypted_A_response) . "\n\n";
+				} else {
+					print rawurlencode($encrypted_A_response);
+				}
+			} else {
+				$rsa->get_time_error();
+			}
 		} else {
-			print "Time expired";
+			$rsa->get_time_error();
 		}
 	} else if(isset($_GET["confirm_friend"]) && trim($_GET["confirm_friend"]) !== "") {
 		if(isset($_GET["debug"])) {
@@ -101,33 +194,38 @@ if(isset($_GET["token"]) && trim($_GET["token"]) !== "") {
 			print "-----\n\n";
 		}
 		
-		$fb = fopen($main_dir . "rsa_2048_pub.pem", "r");
-		$Bkey = fread($fb, 8192);
-		fclose($fb);
-		$password = sha1($Bkey);
-		
 		// B parse
-		$resp = explode("::::", PMA_blowfish_decrypt(rawurldecode($_GET["confirm_friend"]), $password));
-		$Akey = $resp[0];
-		$Btoken = $resp[1];
-		$Atoken = get_token($Akey);
-		$password = sha1($Akey);
-		
-			// B response
-			$encrypted_B_response = rawurlencode(PMA_blowfish_encrypt($Btoken, $password));
-			
-			if(isset($_GET["debug"])) {
-				print "B token is: " . $Btoken . "\n";
-				print "Decrypted A key is:\n" . $Akey . "\n\n";
-				print "A is trusted and its token is: " . $Atoken . "\n\n";
-				print "Encrypted A response:\n" . $encrypted_B_response;
+		$key = explode("::::", rawurldecode($_GET["confirm_friend"]));
+		if(count($key) > 1) {
+			if($rsa->get_time_limit($rsa->simple_decrypt($key[2]))) {
+				$Akey = $rsa->private_decrypt($main_dir, "rsa_2048_priv.pem", $key[1], $key[0], $rsa->simple_decrypt($key[2]));
+				if($Akey) {
+					$Atoken = $rsa->get_token($Akey);
+					$knower = fopen($trusted_dir . $Atoken . ".pem", "wb");
+					fwrite($knower, $Akey);
+					fclose($knower);
+					
+					$encrypted_B_response = $rsa->public_encrypt($main_dir, "trusted/"  . $Atoken . ".pem", "rsa_2048_pub.pem", $rsa->simple_decrypt($key[2]));
+					if($encrypted_B_response) {
+						if(isset($_GET["debug"])) {
+							print "Decrypted A key is:\n" . $Akey . "\n\n";
+							print "A is trusted and its token is: " . $Atoken . "\n\n";
+							print "Encrypted B response:\n" . rawurlencode($encrypted_B_response);
+						} else {
+							print rawurlencode($encrypted_B_response);
+						}
+					} else {
+						$rsa->get_time_error();
+					}
+				} else {
+					$rsa->get_time_error();
+				}
 			} else {
-				print $encrypted_B_response;
+				$rsa->get_time_error();
 			}
-			$knower = fopen($trusted_dir . $Atoken . ".pem", "wb");
-			fwrite($knower, $_SERVER["REMOTE_ADDR"] . "\n\n" . $Akey);
-			fclose($knower);
-		
+		} else {
+			$rsa->get_time_error();
+		}
 	} else if(isset($_GET["reconfirm_friend"]) && trim($_GET["reconfirm_friend"]) !== "") {
 		if(isset($_GET["debug"])) {
 			print "-----\n";
@@ -135,27 +233,43 @@ if(isset($_GET["token"]) && trim($_GET["token"]) !== "") {
 			print "-----\n\n";
 		}
 		
-		$fa = fopen($main_dir . "rsa_2048_pub.pem", "r");
-		$Akey = fread($fa, 8192);
-		fclose($fa);
-		$password = sha1($Akey);
-		
 		// A 2nd parse
-		$resp = explode("::::", PMA_blowfish_decrypt(rawurldecode($_GET["reconfirm_friend"]), $password));
-		$Btoken = $resp[0];
-		$md5_pass = $resp[1];
-		
-			if(isset($_GET["debug"])) {
-				print "B is trusted and its token is: " . $Btoken;
+		$key = explode("::::", rawurldecode($_GET["reconfirm_friend"]));
+		if(count($key) > 1) {
+			if($rsa->get_time_limit($rsa->simple_decrypt($key[2]))) {
+				$Bkey = $rsa->private_decrypt($main_dir, "rsa_2048_priv.pem", $key[1], $key[0], $rsa->simple_decrypt($key[2]));
+				if($Bkey) {
+					$Btoken = $rsa->get_token($Bkey);
+					if(isset($_GET["debug"])) {
+						print "B is trusted and its token is: " . $Btoken;
+					} else {
+						print "Hello " . $Btoken . ", nice to meet you!";
+					}
+					// Check knowers
+					foreach(glob($trusted_dir . "*.pem~") as $filename) {
+						$tokens[] = str_replace(array($trusted_dir, ".pem~"), "", $filename);
+					}
+					if(is_array($tokens)) {
+						if(in_array($Btoken, $tokens)) {
+							// Check_keys
+							$fb = fopen($trusted_dir . $Btoken . ".pem~", "r");
+							$knowerBkey = fread($fb, 8192);
+							fclose($fb);
+							// Rename knower to trusted
+							if(trim($knowerBkey) == trim($Bkey)) {
+								rename($trusted_dir . $Btoken . ".pem~", $trusted_dir . $Btoken . ".pem");
+							}
+						}
+					}
+				} else {
+					$rsa->get_time_error();
+				}
 			} else {
-				print $Btoken;
+				$rsa->get_time_error();
 			}
-			foreach(glob($trusted_dir . "*.pem~") as $filename) {
-				$tokens[] = str_replace(array($trusted_dir, ".pem~"), "", $filename);
-			}
-			if(in_array($Btoken, $tokens)) {
-				rename($trusted_dir . $Btoken . ".pem~", $trusted_dir . $Btoken . ".pem");
-			}
+		} else {
+			$rsa->get_time_error();
+		}
 	} else {
 		print "You must to provide token!";
 	}
