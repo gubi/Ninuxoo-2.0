fa70666e3685a67bb7591ceaba061c1aa918485d
gubi <gubi.ale@iod.io>
2014-02-28 15:53:27 +0100

    Fixed url shortening class

--- common/include/classes/url_shortener.class.php
+++ common/include/classes/url_shortener.class.php
@@ -2,7 +2,7 @@
 //For more info about Yourls' API visit http://yourls.org/#API
 class yourls {
 	function __construct() {
-		$this->yourls_url = "http://nnx.me/yourls-api.php?";
+		$this->yourls_url = "http://nnx.me/yourls-api.php";
 		$this->yourls_token = "KcMk8hjh90y3U8fE3r2ZDl9lzYhRlp5YgnLgy0vVBFVKXrGeDAj1v8x0USay8vYsIMAojjwqrP1Xjx/dgKTAJw==";
 		
 		$this->obj = new stdClass();
@@ -12,7 +12,7 @@ class yourls {
 		$t = base64_decode($this->yourls_token);
 		$k = "shorten";
 		$iv = substr($t, 0, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC));
-		$dec = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $k, true), substr($t, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)), MCRYPT_MODE_CBC, $iv), "\0");
+		$dec = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash("sha256", $k, true), substr($t, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)), MCRYPT_MODE_CBC, $iv), "\0");
 		return $dec;
 	}
 	private function error($message = "Si e' verificato un errore") {
@@ -32,6 +32,18 @@ class yourls {
 		}
 		$this->obj->action = $action;
 	}
+	public function keyword($keyword = "") {
+		if(strlen(trim($keyword)) == 0) {
+			$keyword = "";
+		}
+		$this->obj->keyword = $keyword;
+	}
+	public function title($title = "") {
+		if(strlen(trim($title)) == 0) {
+			$title = "Ninuxoo page";
+		}
+		$this->obj->title = $title;
+	}
 	public function url($url) {
 		if(strlen(trim($url)) > 0) {
 			$this->obj->action = $url;
@@ -46,11 +58,34 @@ class yourls {
 		if(strlen(trim($this->obj->action)) == 0) {
 			$this->action();
 		}
+		if(strlen(trim($this->obj->keyword)) == 0) {
+			$this->keyword();
+		}
+		if(strlen(trim($this->obj->title)) == 0) {
+			$this->title();
+		}
 		if(strlen(trim($url)) == 0) {
 			$this->error("Nessun URL da raccorciare");
 		} else {
-			$this->obj->url = $url;
-			$shorted = file_get_contents($this->yourls_url . http_build_query($this->obj));
+			$this->obj->url = trim($url);
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $this->yourls_url);
+			curl_setopt($ch, CURLOPT_HEADER, 0); // No header in the result
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result
+			curl_setopt($ch, CURLOPT_POST, 1); // This is a POST request
+			curl_setopt($ch, CURLOPT_POSTFIELDS, array(// Data to POST
+				"url" => $this->obj->url,
+				"keyword" => $this->obj->keyword,
+				"title" => $this->obj->title,
+				"format" => $this->obj->format,
+				"action" => $this->obj->action,
+				"signature" => $this->obj->signature
+			));
+
+			// Fetch and return content
+			$shorted = curl_exec($ch);
+			curl_close($ch);
+			//$shorted = file_get_contents($this->yourls_url . http_build_query($this->obj));
 			return $shorted;
 		}
 	}
--- common/include/funcs/_ajax/get_permalink.php
+++ common/include/funcs/_ajax/get_permalink.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: text/plain");
-require_once("../../classes/class.url_shortener.php");
+require_once("../../classes/url_shortener.class.php");
 
 $yourls = new yourls();
 print $yourls->shorten($output["url"]);
